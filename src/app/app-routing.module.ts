bdba780476e66d312fb00a6c48220b97b4f374c3
luizguaycurus@gmail.com <gabriel_calixto.gsc@hotmail.com>
2019-11-13 10:58:51 -0300

    aee

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,6 +6,7 @@ const routes: Routes = [
     path: '',
     loadChildren: () => import('./tabs/tabs.module').then(m => m.TabsPageModule)
   },  { path: 'list-ficha', loadChildren: './pages/list-ficha/list-ficha.module#ListFichaPageModule' },
+  { path: 'perfil-ficha', loadChildren: './pages/perfil-ficha/perfil-ficha.module#PerfilFichaPageModule' },
 
   //{ path: 'add-ficha', loadChildren: './pages/add-ficha/add-ficha.module#AddFichaPageModule' }
 
---
+++ src/app/pages/perfil-ficha/perfil-ficha.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-perfil-ficha',
+  templateUrl: './perfil-ficha.page.html',
+  styleUrls: ['./perfil-ficha.page.scss'],
+})
+export class PerfilFichaPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
